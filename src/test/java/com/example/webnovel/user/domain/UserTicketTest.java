48e5b2e5303a7f6c428e27c68559b59776a122b8
etff <wish2rich@gmail.com>
2023-04-15 22:31:01 +0900

    refactor: 리뷰반영
    - 주석제거

--- src/test/java/com/example/webnovel/user/domain/UserTicketTest.java
+++ src/test/java/com/example/webnovel/user/domain/UserTicketTest.java
@@ -1,30 +1,36 @@
 package com.example.webnovel.user.domain;
 
+import com.example.webnovel.user.exception.InvalidCountException;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class UserTicketTest {
 
-//    @Test
-//    void create() {
-//        UserTicket userTicket = new UserTicket(1L, 5);
-//
-//        assertThat(userTicket.getTicketCount()).isEqualTo(5);
-//    }
-//
-//    @Test
-//    void createWithInvalidCount() {
-//        assertThrows(InvalidCountException.class, () -> new UserTicket(1L, -1));
-//    }
-//
-//    @Test
-//    void changeTotalCount() {
-//        UserTicket userTicket = new UserTicket(1L, 5);
-//        UserTicket changedUserTicket = userTicket.changeTotalCount(1);
-//
-//        assertThat(changedUserTicket.getTicketCount()).isEqualTo(6);
-//    }
-//
-//    @Test
-//    void changeTotalCountWithInvalidCount() {
-//        UserTicket userTicket = new UserTicket(1L, 5);
-//        assertThrows(InvalidCountException.class, () -> userTicket.changeTotalCount(-6));
-//    }
+    @Test
+    void create() {
+        UserTicket userTicket = new UserTicket(1L, 5);
+
+        assertThat(userTicket.getTicketCount()).isEqualTo(5);
+    }
+
+    @Test
+    void createWithInvalidCount() {
+        assertThrows(InvalidCountException.class, () -> new UserTicket(1L, -1));
+    }
+
+    @Test
+    void changeTotalCount() {
+        UserTicket userTicket = new UserTicket(1L, 5);
+
+        assertDoesNotThrow(() -> userTicket.changeTotalCount(1));
+    }
+
+    @Test
+    void changeTotalCountWithInvalidCount() {
+        UserTicket userTicket = new UserTicket(1L, 5);
+        assertThrows(InvalidCountException.class, () -> userTicket.changeTotalCount(-6));
+    }
 }
