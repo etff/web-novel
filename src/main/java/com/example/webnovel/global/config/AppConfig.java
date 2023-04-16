da69daaf7eff4244d6d158180d570b4e2e21a5a8
etff <wish2rich@gmail.com>
2023-04-16 16:18:19 +0900

    chore: querydsl 의존성 추가

--- src/main/java/com/example/webnovel/global/config/AppConfig.java
+++ src/main/java/com/example/webnovel/global/config/AppConfig.java
@@ -1,15 +1,26 @@
 package com.example.webnovel.global.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+    private final EntityManager em;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
 }
